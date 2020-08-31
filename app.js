4768853c98b2e12291f3715ee6e1be8f88f41958
Chance Nguyen <chanceknguyen@gmail.com>
2020-08-31 17:07:06 -0500

    Complete Mini Sprint

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // TODO
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+
+
+  enter() {
+    this.setState({
+      done: true
+    });
+  }
+
+  leave() {
+    this.setState({
+      done: false
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : ''
+    }
+    return (
+      <li style={style} onMouseLeave={this.leave.bind(this)} onMouseEnter={this.enter.bind(this)} >{this.props.groceryItem}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <div>
+    <h2>My Grocery List</h2>
+    <ul>
+      {props.groceryItems.map(groceryItem =>
+        <GroceryListItem groceryItem={groceryItem} />
+        )}
+    </ul>
+  </div>
+);
+
+ReactDOM.render(<GroceryList groceryItems = {['eggs', 'milk', 'apples']}/>, document.getElementById('app'));
+
